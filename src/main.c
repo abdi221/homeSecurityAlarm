6c1c2b21a882276ec16d554b0c8bc07d87d91e6c
abdi221 <abdifatahali22@gmail.com>
2025-08-04 23:11:42 +0200

    Trying out simple freertos test to see if code runs

--- src/main.c
+++ src/main.c
@@ -8,60 +8,101 @@
 #include "temp_display_queue.h"
 
 #define MAIN_LED_DELAY 800
+#define LED_PIN 25
+#define button 0
+#define blink_ms
+#define polls_ms
 
-void led_task(void *pvParameters);
+static void blink_task(void *pvParamters) {
+    (void)pvParamters;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    for(;;) {
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(blink_ms));
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(blink_ms));
 
-void display_task(void *pvParameters);
+    }
+}
 
-void start_tasks();
+static void button_task(void *vParameters) {
+    (void) vParameters;
+    gpio_init(button);
+    gpio_set_dir(button, GPIO_IN);
+    gpio_pull_up(button);
+
+    for (;;) {
+        if (!gpio_get(button)) {
+            printf("freertos is running nigga");
+
+            while (!gpio_get(button)) {
+                vTaskDelay(pdMS_TO_TICKS(polls_ms));
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(polls_ms));
+    }
+}
+
+// void led_task(void *pvParameters);
+
+// void display_task(void *pvParameters);
+
+// void start_tasks();
 
 int main() {
-    stdio_init_all();  // Initialize
+   // Initialize USB serial for printf()
+    stdio_init_all();
+    printf("Starting FreeRTOS button/LED demo...\n");
 
-    printf("Main Program Executation start!\n");
+    // Create the two tasks
+    xTaskCreate(blink_task, "Blink",  256, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(button_task, "Button",256, NULL, tskIDLE_PRIORITY + 2, NULL);
 
-    // start_tasks();
-    create_temp_display_queue_task();
-
-    return 0;
-}
-
-void start_tasks() {
-    // Create Your Task
-    xTaskCreate(
-        led_task,    // Task to be run
-        "LED_TASK",  // Name of the Task for debugging and managing its Task Handle
-        1024,        // Stack depth to be allocated for use with task's stack (see docs)
-        NULL,        // Arguments needed by the Task (NULL because we don't have any)
-        1,           // Task Priority - Higher the number the more priority [max is (configMAX_PRIORITIES - 1) provided in FreeRTOSConfig.h]
-        NULL         // Task Handle if available for managing the task
-    );
-
-    // Should start you scheduled Tasks (such as the LED_Task above)
+    // Start the scheduler (does not return)
     vTaskStartScheduler();
 
-    while (true) {
-        // Your program should never get here
-    };
+    // Should never reach here
+    for (;;) { tight_loop_contents(); }
 }
 
-void display_task(void *pvParameters) {
-    run_display();
-}
+// void start_tasks() {
+//     // Create Your Task
+//     xTaskCreate(
+//         led_task,    // Task to be run
+//         "LED_TASK",  // Name of the Task for debugging and managing its Task Handle
+//         1024,        // Stack depth to be allocated for use with task's stack (see docs)
+//         NULL,        // Arguments needed by the Task (NULL because we don't have any)
+//         1,           // Task Priority - Higher the number the more priority [max is (configMAX_PRIORITIES - 1) provided in FreeRTOSConfig.h]
+//         NULL         // Task Handle if available for managing the task
+//     );
 
-void led_task(void *pvParameters) {
-    bool is_connected = true;
-    if (cyw43_arch_init()) {
-        printf("WiFi init failed\n");
-        is_connected = false;
-    }
+//     // Should start you scheduled Tasks (such as the LED_Task above)
+//     vTaskStartScheduler();
 
-    while (is_connected) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        printf("LED tuned ON!\n");
-        vTaskDelay(MAIN_LED_DELAY);  // Delay by TICKS defined by FreeRTOS priorities
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        printf("LED turned OFF\n");
-        vTaskDelay(MAIN_LED_DELAY);
-    }
-}
+//     while (true) {
+//         // Your program should never get here
+//     };
+// }
+
+// void display_task(void *pvParameters) {
+//     run_display();
+// }
+
+// void led_task(void *pvParameters) {
+//     bool is_connected = true;
+//     if (cyw43_arch_init()) {
+//         printf("WiFi init failed\n");
+//         is_connected = false;
+//     }
+
+//     while (is_connected) {
+//         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+//         printf("LED tuned ON!\n");
+//         vTaskDelay(MAIN_LED_DELAY);  // Delay by TICKS defined by FreeRTOS priorities
+//         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+//         printf("LED turned OFF\n");
+//         vTaskDelay(MAIN_LED_DELAY);
+//     }
+// }
